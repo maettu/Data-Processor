bebba8a9cb1c7239dae00aa810e73df0553d98e7
Matthias Bloch <matthias.bloch@puffin.ch>
2015-01-17 11:18:28 +0100

    Error instance

--- lib/Data/Processor/Error/Instance.pm
+++ lib/Data/Processor/Error/Instance.pm
@@ -4,6 +4,41 @@ use warnings;
 package Data::Processor::Error::Instance;
 
 =head1 NAME
-Data::Processor::Error::Collection - Collect errors
+Data::Processor::Error::Instance - An Error
+
+=head1 METHODS
+=head2 new
+
+  my $error = Data::Processor::Error::Instance->new(
+                message => 'This is an error.',
+                path    => 'root->key->another->key',
+                caller  => "got called by " . caller();
+            );
 =cut
+use overload ('""' => \&stringify);
+
+sub new {
+    my $class = shift;
+    my $self = { @_ };
+    my %keys  = ( map { $_ => 1 } keys %$self );
+    for (qw (message path caller)){
+        delete $keys{$_};
+        $self->{$_} // die "$_ missing";
+    }
+    die "Unknown keys ". join (",",keys %keys) if keys %keys;
+    bless ($self, $class);
+    return $self;
+}
+
+=head2 stringify
+We 'use overload ('""' => \&stringify)' to call this routine when you
+print an error.
+Does not take arguments other than $self.
+=cut
+
+sub stringify {
+    my $self = shift;
+    return $self->{path} . ": " . $self->{message};
+}
+
 1;
