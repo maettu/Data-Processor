4e670f27ad507c0011e4b70b7cd65756af6efdda
Matthias Bloch <matthias.bloch@puffin.ch>
2015-01-17 11:22:29 +0100

    Error Collection

--- lib/Data/Processor/Error/Collection.pm
+++ lib/Data/Processor/Error/Collection.pm
@@ -4,8 +4,40 @@ use warnings;
 package Data::Processor::Error::Collection;
 use Data::Processor::Error::Instance;
 
-
 =head1 NAME
-Data::Processor::Error::Collection - Collect errors
+Data::Processor::Error::Collection - Collect errors for Data::Processor
+
+=head1 METHODS
+=head2 new
+
+    my $errors = Data::Processor::Error::Collection->new();
+
 =cut
+sub new {
+    my $class = shift;
+    my $self = {
+        errors => [] # the error instances are going into here
+    };
+    bless ($self, $class);
+    return $self;
+}
+
+=head2 add
+Adds an error.
+=cut
+sub add {
+    my $self = shift;
+    my %p    = @_;
+    my $error = Data::Structure::Validation::Error::Instance->new(%p);
+    push @{$self->{errors}}, $error;
+}
+
+=head2 as_array
+    Return all collected errors as an array.
+=cut
+sub as_array{
+    my $self = shift;
+    return @{$self->{errors}};
+}
 1;
+
