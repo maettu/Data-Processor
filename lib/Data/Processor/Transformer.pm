073b457b7aa6053f970a92ab4bb9515f6c03e0fe
Tobias Oetiker <tobi@oetiker.ch>
2015-02-16 08:13:25 +0100

    add test and fix transform_data function

--- lib/Data/Processor/Transformer.pm
+++ lib/Data/Processor/Transformer.pm
@@ -18,16 +18,12 @@ sub transform {
     my $key     = shift;
     my $schema_key = shift;
     my $section = shift;
-    if (exists $section->{schema}{$schema_key}
-        and exists $section->{schema}{$schema_key}{transformer}){
-
+    if (exists $section->{schema}{$schema_key}{transformer}){
         my $return_value;
         eval {
             local $SIG{__DIE__};
             $return_value =
-                $section->{schema}{$schema_key}{transformer}
-                ->($section->{data}{$key},$section->{data});
-
+                $section->{schema}{$schema_key}{transformer}($section->{data}{$key},$section->{data});
         };
         if (my $err = $@) {
             if (ref $err eq 'HASH' && defined $err->{msg}){
